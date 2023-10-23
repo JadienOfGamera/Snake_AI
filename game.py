8538b233705d087d02290e3dadf97825bf5d5c4c
Willemy Malik <malik.willemy@alumni.enac.fr>
2023-10-23 15:40:09 +0200

    Now draw the grid

--- game.py
+++ game.py
@@ -5,10 +5,9 @@ import pygame, sys
 #Grid => 0 : Empty | 1 : Apple | 2 : Snake
 
 class Grid:
-    def __init__(self, size_x, size_y, margin, screen):
+    def __init__(self, size_x, size_y, screen):
         self.size_x = size_x
         self.size_y = size_y
-        self.margin = margin
         self.screen = screen
 
         self.grid = [[0 for x in range(size_y)] for y in range(size_x)]
@@ -17,27 +16,39 @@ class Grid:
 
         self.grid[self.apple_pos_x][self.apple_pos_y] = 1
         
-        for case_x in range(0, self.size_y, margin):
-            for case_y in range(0, self.size_x, margin):
+        for case_x in range(self.size_y):
+            for case_y in range(self.size_x):
                 print(self.grid[case_x][case_y], end="")
-                pygame.Rect(case_x, case_y, margin, margin)
-
             print("")
+
+
+    def draw_grid(self):
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                rect = pygame.Rect(x * margin, y * margin, margin, margin)
+                if self.grid[x][y] == 0:
+                    pygame.draw.rect(self.screen, (255, 255, 255), rect, 1)
+                elif self.grid[x][y] == 1:
+                    pygame.draw.rect(self.screen, (255, 0, 0), rect)
+        pygame.display.update()
 
 
 if __name__ == '__main__':
     
-    size_x = 10
-    size_y = 10
-    margin = 20
+    WIDTH = 800
+    HEIGHT = 800
+    size_x = 20
+    size_y = 20
+    margin = 40
     
     pygame.init()
-    screen = pygame.display.set_mode((600, 600))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    grid = Grid(size_x, size_y, margin, screen)
+    grid = Grid(size_x, size_y, screen)
 
     running = True
     while running:
+        grid.draw_grid()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
